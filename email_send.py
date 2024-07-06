0d7146e2804b3b2f3b2882405bbd2221df1f2ad6
Hashir <hashiranees524@gmail.com>
2024-07-07 00:51:20 +0500

    Added Subject, source links

--- email_send.py
+++ email_send.py
@@ -2,13 +2,19 @@ import smtplib , ssl
 from os import getenv
 
 def email_send(message):
-    email = getenv("GMAIL")
+
+    # My contents
+    host_email = getenv("GMAIL")
     password = getenv("GMAILpass")
     host = "smtp.gmail.com"
     port = 465
+
+    # Enter email whom you want to send contents in this list
+    elist = [email]
 
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host,port,context=context) as script:
         script.login(email,password)
-        script.sendmail(email,email, message)
+        for email in elist:
+            script.sendmail(host_email, elist, message)
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
-import os
+import os , time
 import email_send
+
+time = time.strftime("%d-%m-%y")
 
 api = os.getenv("APIkey2")
 
-url = f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={api}"
+url = f"https://gnews.io/api/v4/\
+top-headlines?country=pk&category=general&lauguage=en\
+&apikey={api}"
 
 dic = requests.get(url)
 dic = dic.json()
@@ -14,8 +18,13 @@ for i , articles in enumerate(dic["articles"]):
     if articles["description"] == None:
         continue
     else:
-        body = str(body + articles['title'] + '\n' + articles['description'] + '\n\n')
+        body = str(body + articles['title'] + '\n' 
+        + articles['description'] + '\n' 
+        + articles["url"] + 2*'\n')
 
+# will be uploaded to a server and will be executed at a specific time
+
+body = f"Subject: Daily news {time}\n" + body
 body = body.encode("utf-8")
 
 email_send.email_send(body)
